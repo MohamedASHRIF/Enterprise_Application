a1533e99feced3db44c3e1f08a5ab0b08bc1f4e5
Amri <muhammedamri2002@gmail.com>
2025-09-24 19:29:50 +0530

    jwt

--- auth/authentication/src/main/java/com/enterprise/authentication/filter/JwtAuthenticationFilter.java
+++ auth/authentication/src/main/java/com/enterprise/authentication/filter/JwtAuthenticationFilter.java
@@ -1,18 +1,17 @@
 package com.enterprise.authentication.filter;
 
 import com.enterprise.authentication.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
