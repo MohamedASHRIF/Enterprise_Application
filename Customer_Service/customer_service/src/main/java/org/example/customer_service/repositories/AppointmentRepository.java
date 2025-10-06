df24a9a6ea3ef46ce521df9c6e461bc694468a52
Thajeevan <161084799+ThajeevanSE@users.noreply.github.com>
2025-10-06 22:37:48 +0530

    repository created

--- Customer_Service/customer_service/src/main/java/org/example/customer_service/repositories/AppointmentRepository.java
+++ Customer_Service/customer_service/src/main/java/org/example/customer_service/repositories/AppointmentRepository.java
@@ -1,4 +1,9 @@
 package org.example.customer_service.repositories;
 
-public class AppointmentRepository {
+import org.example.customer_service.entities.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByCustomerId(Long customerId);
 }
--- Customer_Service/customer_service/src/main/java/org/example/customer_service/repositories/VehicleRepository.java
+++ Customer_Service/customer_service/src/main/java/org/example/customer_service/repositories/VehicleRepository.java
@@ -1,4 +1,9 @@
 package org.example.customer_service.repositories;
 
-public class VehicleRepository {
+import org.example.customer_service.entities.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    List<Vehicle> findByCustomerId(Long customerId);
 }
