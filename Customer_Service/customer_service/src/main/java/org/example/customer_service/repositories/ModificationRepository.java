67dfbaccb9dc1c8933195cfe2963f7f11007a7ae
Thajeevan <161084799+ThajeevanSE@users.noreply.github.com>
2025-10-06 22:45:58 +0530

    repository created

--- Customer_Service/customer_service/src/main/java/org/example/customer_service/repositories/ModificationRepository.java
+++ Customer_Service/customer_service/src/main/java/org/example/customer_service/repositories/ModificationRepository.java
@@ -1,4 +1,9 @@
 package org.example.customer_service.repositories;
 
-public class ModificationRepository {
+import org.example.customer_service.entities.Modification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ModificationRepository extends JpaRepository<Modification, Long> {
+    List<Modification> findByCustomerId(Long customerId);
 }
