4077353c62996d5540ac390e370dd6770b421414
nismygaffoor <nismygaffoor5@gmail.com>
2025-11-01 01:09:45 +0530

    Update Assignment.java

--- employee-service/src/main/java/com/enterprise/employee_service/domain/Assignment.java
+++ employee-service/src/main/java/com/enterprise/employee_service/domain/Assignment.java
@@ -5,8 +5,11 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "assignments")
 public class Assignment {
 
@@ -14,13 +17,14 @@ public class Assignment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Reference ID from customer/appointment service
+    @Column(nullable = false)
     private Long appointmentId;
 
-    // Reference to employee from Auth Service
+    @Column(nullable = false)
     private Long employeeId;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private AssignmentStatus status;
 
     private LocalDateTime createdAt;
