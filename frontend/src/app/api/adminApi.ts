19058f869be61bcd4381012ff8bf2b387d8e35fe
Lingaraj198 <lingarajuk198@gmail.com>
2025-11-02 23:23:57 +0530

    after alterations

--- frontend/src/app/api/adminApi.ts
+++ frontend/src/app/api/adminApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const adminApi = axios.create({
-  baseURL: "http://localhost:8081/api",
+  baseURL: "http://localhost:8060/api",
   headers: { "Content-Type": "application/json" },
 });
 
