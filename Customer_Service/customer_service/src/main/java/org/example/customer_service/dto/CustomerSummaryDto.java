2a3ca434d9be0cd956faea7c22543a7824734467
nismygaffoor <nismygaffoor5@gmail.com>
2025-11-03 01:07:44 +0530

    Create CustomerSummaryDto.java

---
+++ Customer_Service/customer_service/src/main/java/org/example/customer_service/dto/CustomerSummaryDto.java
@@ -0,0 +1,18 @@
+package org.example.customer_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerSummaryDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private List<VehicleSummaryDto> vehicles;
+}
